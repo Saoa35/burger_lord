786e74bff75c4f875fa278b489b050ea998d4d51
Saoa35 <saoamah@gmail.com>
2022-11-05 11:29:30 +0100

    created header

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,7 +3,18 @@ import "./App.css";
 function App() {
   return (
     <div className="wrapper">
-      <header></header>
+      <header>
+        <div className="header__container">
+          <div className="header__logo">
+            {/* <img width={38} src={} alt='Logo' /> */}
+            <div>
+              <h1>Burger Lord</h1>
+              <p>best burgers in the world</p>
+            </div>
+          </div>
+          <div className="header__cart"></div>
+        </div>
+      </header>
       <nav></nav>
       <main className="content"></main>
       <footer></footer>
