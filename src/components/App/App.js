96cf281c3f63d9305889830e2371665191bb7c7f
Saoa35 <saoamah@gmail.com>
2022-11-05 11:07:02 +0100

    cleaned App.css

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,14 @@
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="wrapper">
+      <header></header>
+      <nav></nav>
+      <main className="content"></main>
+      <footer></footer>
+    </div>
+  );
 }
 
 export default App;
