4349d6ff11acd306e1b90485d358d39ce0be1c15
Saoa35 <saoamah@gmail.com>
2022-11-05 14:09:21 +0100

    made up categories

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -23,35 +23,60 @@ function App() {
           </div>
         </div>
       </header>
-      <nav></nav>
+
       <main className="content">
-        <div className="card">
-          <img
-            src="https://sawepecomcdn.blob.core.windows.net/bk-web-ordering/BK%20PL/bk_images/oferty_specjalne/lto_texas_bbq_chicken/texas_bbq_chicken.png"
-            alt="Burger"
-          />
-          <h4 className="card__title">Texas BBQ Chicken</h4>
-          <div className="cartBlock__selector">
-            <ul>
-              <li>Standart</li>
-              <li>Double</li>
-            </ul>
-            <ul>
-              <li>Single</li>
-              <li>+ Coca Cola</li>
-              <li>+ Coca Cola and French fries</li>
-            </ul>
+        <div className="container">
+          <div className="content__top">
+            <div className="categories">
+              <ul>
+                <li className="active">All</li>
+                <li>Chicken</li>
+                <li>Pork</li>
+                <li>Plant-Based</li>
+                <li>Cheese</li>
+                <li>Bacon</li>
+              </ul>
+            </div>
+            <div className="sort">
+              <div className="sort__label">
+                <svg />
+                <b>Sorting by</b>
+                <span>popularity</span>
+              </div>
+            </div>
           </div>
-          <div className="card__bottom">
-            <div className="cart__price">from 15 $</div>
-            <div className="add__button">
-              <svg />
-              <span>Add to Cart</span>
-              <i>2</i>
+          <h2 className="content__title">All Burgers</h2>
+          <div className="content__items">
+            <div className="card">
+              <img
+                src="https://sawepecomcdn.blob.core.windows.net/bk-web-ordering/BK%20PL/bk_images/oferty_specjalne/lto_texas_bbq_chicken/texas_bbq_chicken.png"
+                alt="Burger"
+              />
+              <h4 className="card__title">Texas BBQ Chicken</h4>
+              <div className="cartBlock__selector">
+                <ul>
+                  <li>Standart</li>
+                  <li>Double</li>
+                </ul>
+                <ul>
+                  <li>Single</li>
+                  <li>+ Coca Cola</li>
+                  <li>+ Coca Cola and French fries</li>
+                </ul>
+              </div>
+              <div className="card__bottom">
+                <div className="cart__price">from 15 $</div>
+                <div className="add__button">
+                  <svg />
+                  <span>Add to Cart</span>
+                  <i>2</i>
+                </div>
+              </div>
             </div>
           </div>
         </div>
       </main>
+
       <footer></footer>
     </div>
   );
