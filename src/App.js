f30be366d3fb5927e64f140857d5b3041d4f7a58
Saoa35 <saoamah@gmail.com>
2022-11-06 21:49:03 +0100

    created isLoading state

--- src/App.js
+++ src/App.js
@@ -4,14 +4,19 @@ import Header from "./components/Header";
 import Sort from "./components/Sort";
 import "./scss/app.scss";
 import { useEffect, useState } from "react";
+import Skeleton from "./components/Card/Skeleton";
 
 function App() {
   const [items, setItems] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     fetch("https://6367d9abedc85dbc84dd1748.mockapi.io/items")
       .then((res) => res.json())
-      .then((data) => setItems(data))
+      .then((data) => {
+        setItems(data);
+        setIsLoading(false);
+      })
       .catch((error) => {
         console.log(error.mesage);
       });
@@ -29,9 +34,9 @@ function App() {
           </div>
           <h2 className="content__title">All Burgers</h2>
           <div className="content__items">
-            {items.map((obj) => (
-              <Card key={obj.id} {...obj} />
-            ))}
+            {isLoading
+              ? [...Array(6)].map((_, i) => <Skeleton key={i} />)
+              : items.map((obj) => <Card key={obj.id} {...obj} />)}
           </div>
         </div>
       </main>
--- src/components/Card/Skeleton.jsx
+++ src/components/Card/Skeleton.jsx
@@ -1,18 +1,18 @@
 import React from "react";
 import ContentLoader from "react-content-loader";
 
-const Skeleton = (props) => (
+const Skeleton = () => (
   <ContentLoader
-    speed={0}
+    className="card"
+    speed={2}
     width={280}
-    height={450}
+    height={500}
     viewBox="0 0 280 450"
     backgroundColor="#f3f3f3"
     foregroundColor="#ecebeb"
-    {...props}
   >
-    <rect x="6" y="0" rx="25" ry="25" width="240" height="240" />
-    <rect x="47" y="262" rx="0" ry="0" width="160" height="27" />
+    <rect x="15" y="10" rx="25" ry="25" width="230" height="230" />
+    <rect x="49" y="260" rx="0" ry="0" width="160" height="27" />
     <rect x="1" y="308" rx="15" ry="15" width="258" height="85" />
     <rect x="1" y="410" rx="0" ry="0" width="100" height="25" />
     <rect x="64" y="427" rx="0" ry="0" width="0" height="1" />
