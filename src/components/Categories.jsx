1f6c190b41a5b5bfcaaaeadb56059887da712f3c
Saoa35 <saoamah@gmail.com>
2022-11-06 09:37:45 +0100

    created categories arr

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -3,20 +3,31 @@ import { useState } from "react";
 function Categories() {
   const [activeIndex, setActiveIndex] = useState(0);
 
+  const categories = [
+    "All",
+    "Chicken",
+    "Pork",
+    "Plant-Based",
+    "Cheese",
+    "Bacon",
+  ];
+
   const onClickCategory = (i) => {
     setActiveIndex(i);
   };
 
   return (
     <div className="categories">
-      {}
       <ul>
-        <li className="active">All</li>
-        <li>Chicken</li>
-        <li>Pork</li>
-        <li>Plant-Based</li>
-        <li>Cheese</li>
-        <li>Bacon</li>
+        {categories.map((el, index) => (
+          <li
+            key={index}
+            onClick={() => onClickCategory(index)}
+            className={activeIndex ? "active" : ""}
+          >
+            {el}
+          </li>
+        ))}
       </ul>
     </div>
   );
