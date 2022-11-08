d7bc1387cffda2043b98bf68be8877b35876a010
Saoa35 <saoamah@gmail.com>
2022-11-08 21:13:16 +0100

    filled Cart comp.

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,3 +1,27 @@
 export const Cart = () => {
-  return <div>Cart page</div>;
+  return (
+    <div className="cart">
+      <div className="cart__top">
+        <h2 className="content__title">
+          {/* <svg/> */}
+          Cart
+        </h2>
+        <div className="cart__clear">
+          {/* <svg></svg> */}
+          <span>Clear cart</span>
+        </div>
+      </div>
+      <div className="content__items">
+        <div className="cart__item">
+          <div className="cart__item-img">
+            <img alt="Burger" />
+          </div>
+          <div className="cart__item-info">
+            <h3>Colorado King Chicken</h3>
+            <p>Standart,Single</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
