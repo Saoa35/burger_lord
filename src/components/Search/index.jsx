079437f6020d0691dd1ba4f4bbbff71e1dcbe9a0
Saoa35 <saoamah@gmail.com>
2022-11-10 20:55:00 +0100

    stylized Search comp.

--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -2,8 +2,20 @@ import styles from "./Search.module.scss";
 
 export const Search = () => {
   return (
-    <div>
-      <input className={styles.root} placeholder="Serch burger..." />
+    <div className={styles.root}>
+      <svg
+        className={styles.icon}
+        fill="none"
+        height="24"
+        stroke="currentColor"
+        viewBox="0 0 24 24"
+        width="24"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <circle cx="11" cy="11" r="8" />
+        <line x1="21" x2="16.65" y1="21" y2="16.65" />
+      </svg>
+      <input className={styles.input} placeholder="Serch burger..." />
     </div>
   );
 };
