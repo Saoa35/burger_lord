d34d87b800090c5cfa76b20e11cb87b8b22c08ce
Saoa35 <saoamah@gmail.com>
2022-11-13 14:32:15 +0100

    + createSlice to filterSlice

--- src/redux/slices/filterSlice.js
+++ src/redux/slices/filterSlice.js
@@ -1,4 +1,8 @@
+import { createSlice } from "@reduxjs/toolkit";
+
 const initialState = {
   categoryId: 0,
   sort: { name: "popularity", sortProperty: "rating" },
 };
+
+const filterSlice = createSlice({});
