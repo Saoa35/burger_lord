d359ef8b0ccd4383d2f98886bec66fc3e04a06b7
Saoa35 <saoamah@gmail.com>
2022-11-19 21:41:00 +0100

    + props to CartItem

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,5 +1,6 @@
 import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
+import CartItem from "../components/CartItem";
 
 export const Cart = () => {
   const dispatch = useDispatch();
@@ -82,40 +83,41 @@ export const Cart = () => {
           </div>
         </div>
         <div className="content__items">
-          <div className="cart__bottom">
-            <div className="cart__bottom-details">
-              <span>
-                Total burgers: <b>2</b>
-              </span>
-              <span>
-                Total price: <b>10.40 $</b>
-              </span>
-            </div>
-            <div className="cart__bottom-buttons">
-              <Link
-                to="/"
-                className="button button--outline button--add go-back-btn"
+          <CartItem />
+        </div>
+        <div className="cart__bottom">
+          <div className="cart__bottom-details">
+            <span>
+              Total burgers: <b>2</b>
+            </span>
+            <span>
+              Total price: <b>10.40 $</b>
+            </span>
+          </div>
+          <div className="cart__bottom-buttons">
+            <Link
+              to="/"
+              className="button button--outline button--add go-back-btn"
+            >
+              <svg
+                width="8"
+                height="14"
+                viewBox="0 0 8 14"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
               >
-                <svg
-                  width="8"
-                  height="14"
-                  viewBox="0 0 8 14"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    d="M7 13L1 6.93015L6.86175 1"
-                    stroke="#D3D3D3"
-                    strokeWidth="1.5"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                  ></path>
-                </svg>
-                <span>Go Back</span>
-              </Link>
-              <div className="button pay-btn">
-                <span>Buy Now</span>
-              </div>
+                <path
+                  d="M7 13L1 6.93015L6.86175 1"
+                  stroke="#D3D3D3"
+                  strokeWidth="1.5"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                ></path>
+              </svg>
+              <span>Go Back</span>
+            </Link>
+            <div className="button pay-btn">
+              <span>Buy Now</span>
             </div>
           </div>
         </div>
