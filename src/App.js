ff07baa29b9e5410ada18ed1bc220a77562fdbe0
Saoa35 <saoamah@gmail.com>
2022-11-21 21:29:52 +0100

    + MainLayout to App.js

--- src/App.js
+++ src/App.js
@@ -4,14 +4,17 @@ import { Route, Routes } from "react-router-dom";
 import NotFound from "./pages/NotFound";
 import { Cart } from "./pages/Cart";
 import FullBurger from "./pages/FullBurger";
+import MainLayout from "./layouts/MainLayout";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="cart" element={<Cart />} />
-      <Route path="burger/:id" element={<FullBurger />} />
-      <Route path="*" element={<NotFound />} />
+      <Route path="/" element={<MainLayout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="cart" element={<Cart />} />
+        <Route path="burger/:id" element={<FullBurger />} />
+        <Route path="*" element={<NotFound />} />
+      </Route>
     </Routes>
   );
 }
