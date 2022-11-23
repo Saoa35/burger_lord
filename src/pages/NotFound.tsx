2be23e3ef72e165d60e3c6c22ec7fb74824d732a
Saoa35 <saoamah@gmail.com>
2022-11-24 00:32:49 +0100

    changed NotFound comp. to tsx

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,12 +1,6 @@
 import React from "react";
 import { NotFoundBlock } from "../components/NotFoundBlock";
 
-const NotFound: React.FC = () => {
-  return (
-    <>
-      <NotFoundBlock />
-    </>
-  );
-};
+const NotFound: React.FC = () => <NotFoundBlock />;
 
 export default NotFound;
