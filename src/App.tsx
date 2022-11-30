38403215c4cb165c766125e7d1fbacf06caaa88a
Saoa35 <saoamah@gmail.com>
2022-11-30 21:07:49 +0100

    imported lazy to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
-import "./scss/app.scss";
+import React, { lazy } from "react";
 import Home from "./pages/Home";
 import { Route, Routes } from "react-router-dom";
 import NotFound from "./pages/NotFound";
 import { Cart } from "./pages/Cart";
 import FullBurger from "./pages/FullBurger";
 import MainLayout from "./layouts/MainLayout";
+import "./scss/app.scss";
+
+const OtherComponents = lazy(() => import("./pages/Cart"));
 
 function App() {
   return (
