ebbfa9e8114edd95a3ef2a175a374022b80f1f71
Saoa35 <saoamah@gmail.com>
2022-12-01 19:28:06 +0100

    + Link to FullBurger comp.

--- src/pages/FullBurger.tsx
+++ src/pages/FullBurger.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import axios from "axios";
 import { useState, useEffect } from "react";
-import { useNavigate, useParams } from "react-router-dom";
+import { Link, useNavigate, useParams } from "react-router-dom";
 
 const FullBurger: React.FC = () => {
   const { id } = useParams();
@@ -37,6 +37,11 @@ const FullBurger: React.FC = () => {
       <img src={burger.imageUrl} alt="Burger" />
       <h2>{burger.title}</h2>
       <h4>{burger.price} $</h4>
+      <Link to="/">
+        <button className="button button--outline button--add">
+          <span>Back</span>
+        </button>
+      </Link>
     </div>
   );
 };
